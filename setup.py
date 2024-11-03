f115100e32ff2247d22671ddfd8e411ea9a3b7e1
BRooney05 <brian.rooney@uconn.edu>
2024-11-03 15:18:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import pygame as pg
 import Graphics.graphics as gfx
 from settings import screen_width, screen_height, fps
+from sys import exit
 
 clock = pg.time.Clock()
+mouse_pos = (0,0)
 
 screen = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption('Are You The One?')
 pg.display.set_icon(gfx.icon)
 
-mouse_pos = (0,0)
-
-for graphic in gfx.converted_graphics:
-    graphic.convert_alpha()
+# for graphic in gfx.converted_graphics:
+#     graphic = graphic.convert_alpha()
 ##################################################
 
 def tick():
