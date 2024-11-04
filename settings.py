09878f84eb84da73cf24ee9a07277d634bcc3684
BRooney05 <brian.rooney@uconn.edu>
2024-11-04 01:23:52 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,4 +2,5 @@ screen_width, screen_height = 1600, 900
 width_center = int(screen_width/2)
 height_center = int(screen_height/2)
 screen_center = (width_center, height_center)
+num_characters = 12
 fps = 30
