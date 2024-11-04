913eac32511afa5cde54962927ed1cb44910026a
BRooney05 <brian.rooney@uconn.edu>
2024-11-04 01:24:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ from settings import screen_width, screen_height, fps
 from sys import exit
 
 clock = pg.time.Clock()
-mouse_pos = (0,0)
+
+pairs = [("Mark", "GUY #5"), ("Stacy", "GUY #6"), ("GUY #1", "GUY #7"), ("GUY #2", "GUY #8"), ("GUY #3", "GUY #9"), ("GUY #4", "GUY #10")]
 
 screen = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_caption('Are You The One?')
 pg.display.set_icon(gfx.icon)
-
-# for graphic in gfx.converted_graphics:
-#     graphic = graphic.convert_alpha()
-##################################################
 
 def tick():
     # Update frames
