9fcb3adaa4711b8234b00f087ce2410d13b8bf38
BRooney05 <brian.rooney@uconn.edu>
2024-11-04 01:22:59 -0500

    Update graphics.py

--- Graphics/graphics.py
+++ Graphics/graphics.py
@@ -1,9 +1,20 @@
 import pygame as pg
-
-converted_graphics = []
+from settings import screen_width, screen_height
 
 icon = pg.image.load('Graphics/pg_icon.png')
-converted_graphics.append(icon)
 
 title_bg = pg.image.load('Graphics/island_bg.jpg')
-converted_graphics.append(title_bg)
+title_bg = pg.transform.scale(title_bg, (screen_width, screen_height))
+
+
+gs1_bg = pg.image.load('Graphics/enterance_dock.jpg')
+gs1_bg = pg.transform.scale(gs1_bg, (screen_width, screen_height))
+
+boat_png = pg.image.load('Graphics/boat.png')
+boat_png = pg.transform.scale(boat_png, (int(screen_width/4), int(screen_height/4)))
+
+man1_png = pg.image.load('Graphics/man1_updated.png')
+
+woman1_png = pg.image.load('Graphics/woman1_updated.png')
+
+pg_png = pg.image.load('Graphics/placeholder_guy_updated.png')
